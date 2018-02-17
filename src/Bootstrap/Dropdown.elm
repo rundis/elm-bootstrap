39e7725b534cc08729abf7c9acce41c64bc72781
Magnus Rundberget <mrundberget@hotmail.com>
2018-02-17 22:45:01 +0100

    Fix dropdown dropUp and add dropLeft/dropRight

--- src/Bootstrap/Dropdown.elm
+++ src/Bootstrap/Dropdown.elm
@@ -6,6 +6,8 @@ module Bootstrap.Dropdown
         , toggle
         , splitToggle
         , dropUp
+        , dropRight
+        , dropLeft
         , alignMenuRight
         , anchorItem
         , buttonItem
@@ -24,28 +26,31 @@ module Bootstrap.Dropdown
 
 {-| Dropdowns are toggleable, contextual overlays for displaying lists of links and more. They’re toggled by clicking, not by hovering; this is an intentional design decision.
 
-
-
 **Wiring needed**
 
     import Bootstrap.Dropdown as Dropdown
     import Bootstrap.Button as Button
+
+
     -- .. etc
-
-
     -- Model
+
     type alias Model =
         { myDrop1State : Dropdown.State
         , myDrop1State : Dropdown.State
         }
 
+
     -- Msg
+
     type Msg
         = MyDrop1Msg Dropdown.State
         | MyDrop2Msg Dropdown.State
 
+
     -- init
-    init : (Model, Cmd Msg )
+
+    init : ( Model, Cmd Msg )
     init =
         ( { myDrop1State = Dropdown.initialState
           , myDrop2State = Dropdown.initialState
@@ -53,7 +58,9 @@ module Bootstrap.Dropdown
         , Cmd.none
         )
 
+
     -- update
+
     update : Msg -> Model -> ( Model, Cmd msg )
     update msg model =
         case msg of
@@ -67,8 +74,8 @@ module Bootstrap.Dropdown
                 , Cmd.none
                 )
 
-            -- ... and cases for the drop down actions
 
+    -- ... and cases for the drop down actions
 
     subscriptions : Model -> Sub Msg
     subscriptions model =
@@ -76,7 +83,6 @@ module Bootstrap.Dropdown
             [ Dropdown.subscriptions model.myDrop1State MyDrop1Msg
             , Dropdown.subscriptions model.myDrop2State MyDrop2Msg
             ]
-
 
     view : Model -> Html Msg
     view model =
@@ -100,51 +106,61 @@ module Bootstrap.Dropdown
             -- etc
             ]
 
-
 Bootstrap dropdowns are made interactive in Elm by using state and subscription, so there is a little
 bit of wiring involved when using them in your Elm Application.
 
 
-
 # Dropdown
+
 @docs dropdown, toggle, DropdownToggle
 
 
 ## Options
-@docs dropUp, alignMenuRight, attrs, menuAttrs, DropdownOption
+
+@docs dropUp, dropLeft, dropRight, alignMenuRight, attrs, menuAttrs, DropdownOption
+
 
 # Dropdown items
+
 @docs anchorItem, buttonItem, divider, header, DropdownItem
 
 
 # Split dropdown
+
 @docs splitDropdown, splitToggle, SplitToggleConfig, SplitDropdownToggle
 
 
-
 # Required wiring
-@docs subscriptions, initialState, State
 
+@docs subscriptions, initialState, State
 
 -}
 
 import Bootstrap.Button as Button
 import Bootstrap.Internal.Button as ButtonInternal
 import Html
-import Html.Attributes exposing (class, classList, type_, id, href)
+import Html.Attributes exposing (class, classList, type_, id, href, style)
 import Html.Events exposing (onClick, on, onWithOptions)
 import Mouse
 import AnimationFrame
+import Json.Decode as Json
+import DOM
 
 
 {-| Opaque type representing the view state of a Dropdown. You need to store this state
 in your model and it's initialized by [`initialState`](#initialState)
 -}
 type State
-    = State DropDownStatus
+    = State StateRec
+
+type alias StateRec =
+    { status : DropdownStatus
+    , toggleSize : DOM.Rectangle
+    , menuSize : DOM.Rectangle
+    }
 
 
-type DropDownStatus
+type DropdownStatus
     = Open
     | ListenClicks
     | Closed
@@ -152,10 +168,9 @@ type DropDownStatus
 
 {-| The configuration options available for the toggle in a Split Dropdown.
 
-
-* `options` List of Button options for the main button
-* `togglerOptions` List of Button options for the menu toogle
-* `children` List of child elements for the main button
+  - `options` List of Button options for the main button
+  - `togglerOptions` List of Button options for the menu toogle
+  - `children` List of child elements for the main button
 
 **Important**
 You mustn't define an onClick handler as an option in `options`. That will mess up the toggle feature !
@@ -173,13 +188,20 @@ type alias SplitToggleConfig msg =
 type DropdownOption msg
     = Dropup
     | AlignMenuRight
+    | DropToDir DropDirection
     | MenuAttrs (List (Html.Attribute msg))
     | Attrs (List (Html.Attribute msg))
+
+
+type DropDirection
+    = Dropleft
+    | Dropright
 
 
 type alias Options msg =
     { isDropUp : Bool
     , hasMenuRight : Bool
+    , dropDirection : Maybe DropDirection
     , attributes : List (Html.Attribute msg)
     , menuAttrs : List (Html.Attribute msg)
     }
@@ -208,7 +230,11 @@ your main init function
 -}
 initialState : State
 initialState =
-    State Closed
+    State
+        { status = Closed
+        , toggleSize = DOM.Rectangle 0 0 0 0
+        , menuSize = DOM.Rectangle 0 0 0 0
+        }
 
 
 {-| Option to show the dropdown menu above the dropdown rather than the default which is below
@@ -221,10 +247,23 @@ dropUp =
 {-| Option to align the dropdown menu to the right of the dropdown button.
 
 **NOTE!** Dropdowns are positioned only with CSS and may need some additional styles for exact alignment.
+
 -}
 alignMenuRight : DropdownOption msg
 alignMenuRight =
     AlignMenuRight
+
+{-| Show menu to the right of the button.
+-}
+dropRight : DropdownOption msg
+dropRight =
+    DropToDir <| Dropright
+
+{-| Show menu to the left of the button.
+-}
+dropLeft : DropdownOption msg
+dropLeft =
+    DropToDir <| Dropleft
 
 
 {-| Use this function when you need the customize the Dropdown root div with additional Html.Attribute (s).
@@ -255,23 +294,24 @@ It takes the current (view) state and a configuration record as parameters.
             ]
         }
 
-* `state` The current view state of the dropdown
-* Configuration
-  * `toggleMsg` A `msg` function that takes a state and returns a msg
-  * `toggleButton` The actual button for the dropdown
-  * `options` General display [`options`](#options) for Dropdown widget
-  * `items` List of menu items for the dropdown
+  - `state` The current view state of the dropdown
+  - Configuration
+      - `toggleMsg` A `msg` function that takes a state and returns a msg
+      - `toggleButton` The actual button for the dropdown
+      - `options` General display [`options`](#options) for Dropdown widget
+      - `items` List of menu items for the dropdown
 
 -}
 dropdown :
     State
-    -> { toggleMsg : State -> msg
-       , toggleButton : DropdownToggle msg
-       , options : List (DropdownOption msg)
-       , items : List (DropdownItem msg)
-       }
+    ->
+        { toggleMsg : State -> msg
+        , toggleButton : DropdownToggle msg
+        , options : List (DropdownOption msg)
+        , items : List (DropdownItem msg)
+        }
     -> Html.Html msg
-dropdown ((State status) as state) { toggleMsg, toggleButton, items, options } =
+dropdown ((State {status}) as state) { toggleMsg, toggleButton, items, options } =
     let
         (DropdownToggle buttonFn) =
             toggleButton
@@ -280,21 +320,68 @@ dropdown ((State status) as state) { toggleMsg, toggleButton, items, options } =
             toConfig options
     in
         Html.div
-            ([ classList
-                [ ( "btn-group", True )
-                , ( "show", status /= Closed )
-                , ( "dropup", config.isDropUp )
-                ]
-             ]
-                ++ config.attributes
-            )
+            (dropdownAttributes status config)
             [ buttonFn toggleMsg state
-            , Html.div
+            , dropdownMenu state config items
+            ]
+
+
+dropdownAttributes : DropdownStatus -> Options msg -> List (Html.Attribute msg)
+dropdownAttributes status config =
+    [ classList
+        [ ( "btn-group", True )
+        , ( "show", status /= Closed )
+        , ( "dropup", config.isDropUp )
+        ]
+    ]
+        ++ dropDir config.dropDirection
+        ++ config.attributes
+
+
+dropDir : Maybe DropDirection -> List (Html.Attribute msg)
+dropDir maybeDir =
+    let
+        toAttrs dir =
+            [ class <|
+                "drop"
+                    ++ case dir of
+                        Dropleft ->
+                            "left"
+
+                        Dropright ->
+                            "right"
+            ]
+    in
+        Maybe.map toAttrs maybeDir
+            |> Maybe.withDefault []
+
+
+
+dropdownMenu :
+    State
+    -> Options msg
+    -> List (DropdownItem msg)
+    -> Html.Html msg
+dropdownMenu (State {status, menuSize} as state) config items =
+    let
+        wrapperStyle =
+            if status == Closed then
+                [ ( "height", "0" )
+                , ( "overflow", "hidden" )
+                , ( "position", "relative" )
+                ]
+            else
+                [ ( "position", "relative" ) ]
+    in
+        Html.div
+            [ style wrapperStyle ]
+            [ Html.div
                 ([ classList
                     [ ( "dropdown-menu", True )
                     , ( "dropdown-menu-right", config.hasMenuRight )
-                    , ( "show", status /= Closed )
+                    , ( "show", True )
                     ]
+                 , menuStyle state config
                  ]
                     ++ config.menuAttrs
                 )
@@ -302,10 +389,41 @@ dropdown ((State status) as state) { toggleMsg, toggleButton, items, options } =
             ]
 
 
+menuStyle : State -> Options msg -> Html.Attribute msg
+menuStyle (State {status, toggleSize, menuSize}) config =
+    let
+        default
+            = [ ( "top", "0" ), ( "left", "0" ) ]
+        px n =
+            toString n ++ "px"
+
+        translate x y z =
+            "translate3d("
+                ++ px x ++ ","
+                ++ px y ++ ","
+                ++ px z ++ ")"
+    in
+        style <|
+         case (config.isDropUp, config.dropDirection) of
+            (True, _) ->
+                default ++ [("transform", translate -toggleSize.width -menuSize.height 0)]
+
+            (_, Just Dropright) ->
+                default
+
+            (_, Just Dropleft) ->
+                default ++ [("transform", translate (-toggleSize.width - menuSize.width) 0 0)]
+
+            _ ->
+                default ++ [("transform", translate -toggleSize.width toggleSize.height 0)]
+
+
+
 {-| Function to construct a toggle for a [`dropdown`](#dropdown)
 
-* buttonOptions List of button options for styling the button
-* children List of child elements
+  - buttonOptions List of button options for styling the button
+  - children List of child elements
+
 -}
 toggle :
     List (Button.Option msg)
@@ -327,7 +445,7 @@ togglePrivate buttonOptions children toggleMsg state =
         (ButtonInternal.buttonAttributes buttonOptions
             ++ [ class "dropdown-toggle"
                , type_ "button"
-               , onClick <| toggleOpen toggleMsg state
+               , on "click" (clickHandler toggleMsg state)
                ]
         )
         children
@@ -335,22 +453,24 @@ togglePrivate buttonOptions children toggleMsg state =
 
 {-| Creates a split dropdown. Contains a normal button and a toggle button that are placed next to each other.
 
-* `state` The current view state of the split dropdown
-* Configuration
-  * `toggleMsg` A `msg` function that takes a state and returns a msg
-  * `toggleButton` The actual split button for the dropdown
-  * `options` General display [`options`](#options) for Split dropdown widget
-  * `items` List of menu items for the dropdown
+  - `state` The current view state of the split dropdown
+  - Configuration
+      - `toggleMsg` A `msg` function that takes a state and returns a msg
+      - `toggleButton` The actual split button for the dropdown
+      - `options` General display [`options`](#options) for Split dropdown widget
+      - `items` List of menu items for the dropdown
+
 -}
 splitDropdown :
     State
-    -> { toggleMsg : State -> msg
-       , toggleButton : SplitDropdownToggle msg
-       , options : List (DropdownOption msg)
-       , items : List (DropdownItem msg)
-       }
+    ->
+        { toggleMsg : State -> msg
+        , toggleButton : SplitDropdownToggle msg
+        , options : List (DropdownOption msg)
+        , items : List (DropdownItem msg)
+        }
     -> Html.Html msg
-splitDropdown ((State status) as state) { toggleMsg, toggleButton, items, options } =
+splitDropdown ((State {status}) as state) { toggleMsg, toggleButton, items, options } =
     let
         (SplitDropdownToggle buttonsFn) =
             toggleButton
@@ -359,30 +479,13 @@ splitDropdown ((State status) as state) { toggleMsg, toggleButton, items, option
             toConfig options
     in
         Html.div
-            ([ classList
-                [ ( "btn-group", True )
-                , ( "show", status /= Closed )
-                , ( "dropup", config.isDropUp )
-                ]
-             ]
-                ++ config.attributes
-            )
+            (dropdownAttributes status config)
             (buttonsFn toggleMsg state
-                ++ [ Html.div
-                        ([ classList
-                            [ ( "dropdown-menu", True )
-                            , ( "dropdown-menu-right", config.hasMenuRight )
-                            , ( "show", status /= Closed )
-                            ]
-                         ]
-                            ++ config.menuAttrs
-                        )
-                        (List.map (\(DropdownItem x) -> x) items)
-                   ]
+                ++ [ dropdownMenu state config items ]
             )
 
 
-{-| Function to construct a split button toggle for a  [`splitDropdown`](#splitDropdown)
+{-| Function to construct a split button toggle for a [`splitDropdown`](#splitDropdown)
 
     Dropdown.splitToggle
         { options =
@@ -398,8 +501,7 @@ splitDropdown ((State status) as state) { toggleMsg, toggleButton, items, option
         , [ text "My Dropdown"]
         }
 
-
-* `config` Configuration for the split toggle as described in [`SplitToggleConfig`](#SplitToggleConfig)
+  - `config` Configuration for the split toggle as described in [`SplitToggleConfig`](#SplitToggleConfig)
 
 -}
 splitToggle : SplitToggleConfig msg -> SplitDropdownToggle msg
@@ -422,7 +524,7 @@ splitToggleButtonPrivate { options, togglerOptions, children } toggleMsg state =
             ++ [ class "dropdown-toggle"
                , class "dropdown-toggle-split"
                , type_ "button"
-               , onClick <| toggleOpen toggleMsg state
+               , on "click" (clickHandler toggleMsg state)
                ]
         )
         []
@@ -439,6 +541,7 @@ defaultOptions =
     { hasMenuRight = False
     , isDropUp = False
     , attributes = []
+    , dropDirection = Nothing
     , menuAttrs = []
     }
 
@@ -455,14 +558,18 @@ applyModifier option options =
         Attrs attrs ->
             { options | attributes = attrs }
 
+        DropToDir dir ->
+            { options | dropDirection = Just dir }
+
         MenuAttrs attrs ->
             { options | menuAttrs = attrs }
 
 
 {-| Creates an `a` element appropriate for use in dropdowns
 
-* `attributes` List of attributes
-* `children` List of child elements
+  - `attributes` List of attributes
+  - `children` List of child elements
+
 -}
 anchorItem : List (Html.Attribute msg) -> List (Html.Html msg) -> DropdownItem msg
 anchorItem attributes children =
@@ -472,8 +579,9 @@ anchorItem attributes children =
 
 {-| Creates a `button` element appropriate for use in dropdowns
 
-* `attributes` List of attributes
-* `children` List of child elements
+  - `attributes` List of attributes
+  - `children` List of child elements
+
 -}
 buttonItem : List (Html.Attribute msg) -> List (Html.Html msg) -> DropdownItem msg
 buttonItem attributes children =
@@ -484,8 +592,9 @@ buttonItem attributes children =
 {-| Creates divider element appropriate for use in dropdowns.
 Handy when you want to visually separate groups of menu items in a dropdown menu
 
-* `attributes` List of attributes
-* `children` List of child elements
+  - `attributes` List of attributes
+  - `children` List of child elements
+
 -}
 divider : DropdownItem msg
 divider =
@@ -496,8 +605,9 @@ divider =
 {-| Creates an header element appropriate for use in dropdowns.
 Handy when you want to provide a heading for a group of menu items in a dropdown menu
 
-* `attributes` List of attributes
-* `children` List of child elements
+  - `attributes` List of attributes
+  - `children` List of child elements
+
 -}
 header : List (Html.Html msg) -> DropdownItem msg
 header children =
@@ -510,42 +620,77 @@ header children =
 {-| The dropdowns makes use of subscriptions to ensure that opened dropdowns are
 automatically closed when you click outside them.
 
-    -- In your Main.elm or something similar
-
     subscriptions : Model -> Sub Msg
     subscriptions model =
         Sub.batch
             [ Dropdown.subscriptions model.myDrop1State MyDrop1Msg
             , Dropdown.subscriptions model.myDrop2State MyDrop2Msg
+
             -- etc one for each dropdown (dropdown, navDropdown or splitDropdown)
             ]
 
 -}
 subscriptions : State -> (State -> msg) -> Sub msg
-subscriptions (State status) toMsg =
+subscriptions (State {status} as state) toMsg =
     case status of
         Open ->
             AnimationFrame.times
-                (\_ -> toMsg <| State ListenClicks)
+                (\_ -> toMsg <| updateStatus ListenClicks state)
 
         ListenClicks ->
             Mouse.clicks
-                (\_ -> toMsg <| State Closed)
+                (\_ -> toMsg <| updateStatus Closed state)
 
         Closed ->
             Sub.none
 
 
+
+clickHandler : (State -> msg) -> State -> Json.Decoder msg
+clickHandler toMsg (State {status} as state) =
+    sizeDecoder
+        |> Json.andThen
+            (\(b,m) ->
+                Json.succeed <|
+                    toMsg <|
+                        State
+                            { status = nextStatus status
+                            , toggleSize = b
+                            , menuSize = m
+                            }
+            )
+
+
+sizeDecoder : Json.Decoder (DOM.Rectangle, DOM.Rectangle)
+sizeDecoder =
+    Json.map2 (,)
+      (DOM.target <| DOM.boundingClientRect)
+      (DOM.target <|
+        DOM.nextSibling <|
+            DOM.childNode 0 <|
+                DOM.boundingClientRect)
+
+
 toggleOpen : (State -> msg) -> State -> msg
-toggleOpen toMsg (State status) =
+toggleOpen toMsg ((State { status }) as state) =
     toMsg <|
-        State <|
-            case status of
-                Open ->
-                    Closed
+        updateStatus
+            (nextStatus status)
+            state
 
-                ListenClicks ->
-                    Closed
+nextStatus : DropdownStatus -> DropdownStatus
+nextStatus status =
+    case status of
+        Open ->
+            Closed
 
-                Closed ->
-                    Open
+        ListenClicks ->
+            Closed
+
+        Closed ->
+            Open
+
+
+updateStatus : DropdownStatus -> State -> State
+updateStatus status (State stateRec) =
+    State {stateRec | status = status }
