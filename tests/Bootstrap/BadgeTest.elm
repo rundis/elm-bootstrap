d4ce5aef3b80d883cebe2c0cb11a0ea0ca872bc8
David Seebacher <dseebacher@gmail.com>
2018-07-06 10:37:48 +0200

    Refactor badge option tests to use a default test structure

--- tests/Bootstrap/BadgeTest.elm
+++ tests/Bootstrap/BadgeTest.elm
@@ -15,6 +15,11 @@ badge =
     let
         html =
             Badge.badgeDanger [] [ Html.text "X" ]
+
+        expectBadgeClass constructor class =
+            constructor [] [ Html.text "X" ]
+                |> Query.fromHtml
+                |> Query.has [ classes [ "badge", class ] ]
     in
         describe "Badge with options"
             [ test "expect span and text" <|
@@ -23,23 +28,23 @@ badge =
                         |> Query.fromHtml
                         |> Query.has [ tag "span", text "X" ]
             , describe "All options" <|
-                List.map
-                    (\( constructor, class ) ->
-                        test ("Option " ++ class) <|
-                            \() ->
-                                constructor [] [ Html.text "X" ]
-                                    |> Query.fromHtml
-                                    |> Query.has [ classes [ "badge", class ] ]
-                    )
-                    [ ( Badge.badgePrimary, "badge-primary" )
-                    , ( Badge.badgeSecondary, "badge-secondary" )
-                    , ( Badge.badgeSuccess, "badge-success" )
-                    , ( Badge.badgeInfo, "badge-info" )
-                    , ( Badge.badgeWarning, "badge-warning" )
-                    , ( Badge.badgeDanger, "badge-danger" )
-                    , ( Badge.badgeLight, "badge-light" )
-                    , ( Badge.badgeDark, "badge-dark" )
-                    ]
+                [ test "Expect primary" <|
+                    \() -> expectBadgeClass Badge.badgePrimary "badge-primary"
+                , test "Expect secondary" <|
+                    \() -> expectBadgeClass Badge.badgeSecondary "badge-secondary"
+                , test "Expect success" <|
+                    \() -> expectBadgeClass Badge.badgeSuccess "badge-success"
+                , test "Expect info" <|
+                    \() -> expectBadgeClass Badge.badgeInfo "badge-info"
+                , test "Expect warning" <|
+                    \() -> expectBadgeClass Badge.badgeWarning "badge-warning"
+                , test "Expect danger" <|
+                    \() -> expectBadgeClass Badge.badgeDanger "badge-danger"
+                , test "Expect light" <|
+                    \() -> expectBadgeClass Badge.badgeLight "badge-light"
+                , test "Expect dark" <|
+                    \() -> expectBadgeClass Badge.badgeDark "badge-dark"
+                ]
             ]
 
 
